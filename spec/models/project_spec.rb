4c1560179c6e209ca81fc1575d95cb6691361ea8
olezhek85 <olezhek_85@list.ru>
2016-05-27 23:52:35 +0300

    add second test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,9 +1,25 @@
 require_relative '../../app/models/project'
+require_relative '../../app/models/task'
 require 'rails_helper'
 
 RSpec.describe Project do
-  it 'considers a project with no task to be done' do
-    project = Project.new
-    expect(project.done?).to be_truthy 
+  describe 'initialization' do
+    let(:project) { Project.new }
+    let(:task) { Task.new }
+
+    it 'considers a project with no task to be done' do
+      expect(project).to be_done
+    end
+
+    it 'knows that a project with an incomplete task is not done' do
+      project.tasks << task
+      expect(project).not_to be_done
+    end
+
+    it 'mark a project done if its tasks are done' do
+      project.tasks << task
+      task.mark_completed
+      expect(project).to be_done
+    end
   end
 end
